eb191fa4d35d9c37b82737a9e40b6fb52362ab04
Drayano <houari.rezk.1999@gmail.com>
2023-06-17 00:35:34 +0100

    Project config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
